a061eaa7e7c09bcdb658ae8fcf68861b79117025
ZhengxiLI <zhengxi.li.19@neoma-bs.com>
2020-03-31 22:44:36 +0200

    add new translate

--- src/app/dialog/address-dialog/i18n/cn.ts
+++ src/app/dialog/address-dialog/i18n/cn.ts
@@ -1,60 +1,20 @@
 export const locale = {
     lang: 'cn',
     data: {
-        'address': {
-           'Email':'邮箱',
-           'Phone':'电话',
-           'Siret':'Siret',
-           'Company':'公司',
-           'OrderDetails':'订单详情',
-           'Products':'商品',
-           'Invoice':'发票',
-           'Customer':'顾客',
-           'ShippingAddress':'运送地址',
-           'Modify':'修改',
-           'FirstLineAddress':'地址',
-           'SecondLineAddress':'地址补充',
-           'City':'城市',
-           'ZipCode':'邮编',
-           'Telephone':'电话',
-           'Fax':'传真',
-           'InvoiceAddress':'发票地址',
-           'OrderStatus':'订单状态',
-           'Status':'状态',
-           'UpdatedOn':'更新于',
-           'UpdateStatus':'更新状态',
-           'Payment':'支付',
-           'TransactionID':'交易号',
-           'PaymentMethod':'支付方式',
-           'Amount':'总额',
-           'Date':'日期',
-           'Shipping':'快递',
-           'TrackingCode':'快递单号',
-           'Weight':'重量',
-           'Fee':'费用',
-           'ID':'ID',
-           'Image':'照片',
-           'Name':'名称',
-           'Price':'价格',
-           'Quantity':'数量',
-           'Total':'总额',
-           'INVOICE':'发票',
-           'PRODUCT':'产品',
-           'SUBTOTAL':'总计',
-           'TAX':'税',
-           'DISCOUNT':'折扣',
-           'PleasePayWithin15Days_ThankYouForYourBusiness':'请于15日内支付您的订单,谢谢您的合作.',
-           'small-note':'小提示',
-           'OldStatus':'旧状态',
-           'Operator':'操作员'
-        
-
-
-
-
-
-
-           
-        }
+    'address': {
+        'Add/Modify':'添加/更改',
+        'Entreprisename':'公司名称',
+        'LastName':'姓',
+        'FirstName':'名',
+        'Address':'地址',
+        'AddressComplet':'补充地址',
+        'City':'城市',
+        'Country':'国家',
+        'ZipCode':'邮编',
+        'PhoneNumber':'电话号码',
+        'FAX':'传真号',
+        'Cancel':'取消',
+        'Save':'保存'
     }
+}
 };
